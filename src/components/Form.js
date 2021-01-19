bdba823fdac42b8a2b845080b5d2581ecec7138d
casperleerink <casperleerink@gmail.com>
2021-01-18 21:40:59 -0800

    form debug 1

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,52 +1,22 @@
 import React from "react"
-// import axios from "axios"
-// import * as qs from "query-string"
 import style from "./Form.module.scss"
 
 function Form() {
-  // const [feedbackMsg, setFeedbackMsg] = useState(null)
-  // const formRef = useRef(null)
-  // const fieldRefs = useRef({})
-
-  // const handleSubmit = e => {
-  //   e.preventDefault()
-  //   const formData = {}
-  //   Object.keys(fieldRefs.current).map(
-  //     key => (formData[key] = fieldRefs.current[key].value)
-  //   )
-  //   const axiosOptions = {
-  //     url: location.pathname,
-  //     method: "post",
-  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //     data: qs.stringify(formData),
-  //   }
-  //   axios(axiosOptions)
-  //     .then(response => {
-  //       fieldRefs.current["email"].value = ""
-  //       fieldRefs.current["message"].value = ""
-  //       setFeedbackMsg("Success!")
-  //     })
-  // 		.catch(err =>
-  // 			console.log(err);
-  //       setFeedbackMsg("Helaas is er iets fout gegaan tijdens het verzenden.")
-  //     )
-  // }
   return (
     <form
-      className={style.form}
-      name="contact"
-      method="POST"
       data-netlify="true"
-      action="/success"
+      className={style.form}
+      name="Contact Form"
+      method="POST"
     >
-      <input type="hidden" name="form-name" value="contact" />
+      <input type="hidden" name="form-name" value="Contact Form" />
       <label>
         E-mail
-        <input type="text" name="email" />
+        <input type="email" name="email" />
       </label>
       <label>
         Vraag of Opmerking
-        <textarea name="message" />
+        <textarea name="message" rows="6" maxLength="10000" />
       </label>
       <button type="submit">Verzenden</button>
     </form>
